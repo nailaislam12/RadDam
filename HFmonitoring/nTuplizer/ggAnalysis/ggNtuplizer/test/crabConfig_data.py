6442863ae36875b08fae9b4df24453dbcdcb2391
James Natoli <james.vincent.natoli@cern.ch>
2023-09-10 18:44:07 +0200

    Run3 files and new http protocol

--- HFmonitoring/nTuplizer/ggAnalysis/ggNtuplizer/test/crabConfig_data.py
+++ HFmonitoring/nTuplizer/ggAnalysis/ggNtuplizer/test/crabConfig_data.py
@@ -1,10 +1,11 @@
 if __name__ == '__main__':
 
 # Usage : python crabConfig.py (to create jobs)
-#         ./multicrab -c status -d <work area> (to check job status)
+#         ./multicrab -c status -w <work area> (to check job status)
 
+    import os
     from CRABAPI.RawCommand import crabCommand
-    from httplib import HTTPException
+    from http.client import HTTPException
 
     from CRABClient.UserUtilities import config
     config = config()
@@ -15,58 +16,72 @@ if __name__ == '__main__':
 
     config.General.workArea     = 'DataJobs'
     config.General.transferLogs = False
-    config.JobType.maxMemoryMB = 5000
+    config.JobType.maxMemoryMB = 2000
     config.JobType.maxJobRuntimeMin = 2750
     config.JobType.pluginName   = 'Analysis' 
     config.JobType.psetName     = 'run_data.py'
     config.JobType.sendExternalFolder = True
     config.Data.inputDBS        = 'global'    
     config.Data.splitting       = 'LumiBased' 
-    config.Data.lumiMask        = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt' # 58.83 /fb
+    # Change this...
+    # config.Data.lumiMask        = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt' # 58.83 /fb
+    config.Data.lumiMask        = '/afs/cern.ch/work/j/jnatoli/private/HFCalib/CMSSW_12_3_7/src/RadDam/HFmonitoring/nTuplizer/ggAnalysis/ggNtuplizer/test/LumiMasks/Cert_Collisions2022_355100_362760_Golden.json'
     config.Data.unitsPerJob     = 20
     config.Data.ignoreLocality  = False
     config.Data.publication     = False
     config.Data.allowNonValidInputDataset     = True
-    config.Site.storageSite     = 'T3_US_Rutgers'
+    config.Site.storageSite     = 'T2_US_Wisconsin'
+    # config.Site.storageSite     = 'T3_CH_CERNBOX'
+
+    # Little sanity check...
+    cmssw = os.getenv("CMSSW_BASE")
+    proxy = os.getenv("X509_USER_PROXY")
+    print(f"Using: {cmssw}")
+    print(f"Proxy: {proxy}")
 
     def submit(config):
         try:
             crabCommand('submit', config = config)
-        except HTTPException, hte:
-            print hte.headers
+        except HTTPException as hte:
+            print(hte.headers)
 
     # dataset dependent configuration
+    # DAS SEARCH "dataset=/EGamma/Run2022*/MINIAOD"
 
+    # Run2022A is all commissioning
 
-    # Run2018A
-    config.General.requestName = 'EGamma_Run2018A'
-    config.Data.inputDataset   = '/EGamma/Run2018A-17Sep2018-v2/MINIAOD'
-    config.Data.outLFNDirBase  = '/store/user/mheindl/2018HF/'
+    # Run2022B
+    # config.General.requestName = 'EGamma_Run2022B'
+    # config.Data.inputDataset   = '/EGamma/Run2022B-10Dec2022-v1/MINIAOD'
+    # config.Data.outLFNDirBase  = '/store/user/jnatoli/2022HF/'
+    # p = Process(target=submit, args=(config,))
+    # p.start()
+    # p.join()
+
+    # Run2022C
+    config.General.requestName = 'EGamma_Run2022C'
+    config.Data.inputDataset   = '/EGamma/Run2022C-10Dec2022-v1/MINIAOD'
+    config.Data.outLFNDirBase  = '/store/user/jnatoli/2022HF/'
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
+
+    # Run2022D
+    config.General.requestName = 'EGamma_Run2022D'
+    config.Data.inputDataset   = '/EGamma/Run2022D-10Dec2022-v1/MINIAOD'
+    config.Data.outLFNDirBase  = '/store/user/jnatoli/2022HF/'
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+
+    # E, F, and G only have PromptReco as of April 27, 2023
+
+    # Run2022B, trying the PromptReco set?
+    # config.General.requestName = 'EGamma_Run2022B'
+    # config.Data.inputDataset   = '/EGamma/Run2022B-PromptReco-v1/MINIAOD'
+    # config.Data.outLFNDirBase  = '/store/user/jnatoli/2022HF/'
+    # p = Process(target=submit, args=(config,))
+    # p.start()
+    # p.join()
+
     
-    # Run2018B
-    config.General.requestName = 'EGamma_Run2018B'
-    config.Data.inputDataset   = '/EGamma/Run2018B-17Sep2018-v1/MINIAOD'
-    config.Data.outLFNDirBase  = '/store/user/mheindl/2018HF/'
-    p = Process(target=submit, args=(config,))
-    p.start()
-    p.join()
-
-    # Run2018C
-    config.General.requestName = 'EGamma_Run2018C'
-    config.Data.inputDataset   = '/EGamma/Run2018C-17Sep2018-v1/MINIAOD'
-    config.Data.outLFNDirBase  = '/store/user/mheindl/2018HF/'
-    p = Process(target=submit, args=(config,))
-    p.start()
-    p.join()
-    
-    # Run2018D    
-    config.General.requestName = 'EGamma_Run2018D'
-    config.Data.inputDataset   = '/EGamma/Run2018D-PromptReco-v2/MINIAOD'
-    config.Data.outLFNDirBase  = '/store/user/mheindl/2018HF/'
-    p = Process(target=submit, args=(config,))
-    p.start()
-    p.join()
-
--- HFmonitoring/nTuplizer/ggAnalysis/ggNtuplizer/test/crabConfig_mc.py
+++ HFmonitoring/nTuplizer/ggAnalysis/ggNtuplizer/test/crabConfig_mc.py
@@ -1,10 +1,11 @@
 if __name__ == '__main__':
 
 # Usage : python crabConfig.py (to create jobs)
-#         ./multicrab -c status -d <work area> (to check job status)
+#         ./multicrab -c status -w <work area> (to check job status)
 
+    import os
     from CRABAPI.RawCommand import crabCommand
-    from httplib import HTTPException
+    from http.client import HTTPException
 
     from CRABClient.UserUtilities import config
     config = config()
@@ -25,19 +26,32 @@ if __name__ == '__main__':
     config.Data.unitsPerJob     = 10
     config.Data.ignoreLocality  = False
     config.Data.publication     = False
-    config.Site.storageSite     = 'T3_US_Rutgers'
+    config.Site.storageSite     = 'T2_US_Wisconsin'
+
+    # Little sanity check...
+    cmssw = os.getenv("CMSSW_BASE")
+    print(f"Using: {cmssw}")
 
     def submit(config):
         try:
             crabCommand('submit', config = config)
-        except HTTPException, hte:
-            print hte.headers
+        except HTTPException as hte:
+            print(hte.headers)
 
     # dataset dependent configuration
-    
-    config.General.requestName = 'DYJetsToLL_M-50_Fall18'
-    config.Data.inputDataset   = '/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall18MiniAOD-102X_upgrade2018_realistic_v12_ext1-v1/MINIAODSIM'
-    config.Data.outLFNDirBase  = '/store/user/mheindl/2018HF/'
+    # DAS SEARCH "dataset=/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3*/MINIAODSIM"
+    # Part 1
+    # config.General.requestName = 'DYJetsToLL_M-50_Winter22'
+    # config.Data.inputDataset   = '/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Winter22MiniAOD-122X_mcRun3_2021_realistic_v9_ext1-v1/MINIAODSIM'
+    # config.Data.outLFNDirBase  = '/store/user/jnatoli/2022HF/'
+    # p = Process(target=submit, args=(config,))
+    # p.start()
+    # p.join()
+
+    # Part 2
+    config.General.requestName = 'DYJetsToLL_M-50_Winter22_v2'
+    config.Data.inputDataset   = '/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Winter22MiniAOD-122X_mcRun3_2021_realistic_v9_ext2-v2/MINIAODSIM'
+    config.Data.outLFNDirBase  = '/store/user/jnatoli/2022HF/'
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
