dd642ee35e761233b29a5067a1777b3b7a146335
James Natoli <james.vincent.natoli@cern.ch>
2023-09-10 18:48:41 +0200

    Run3 Input Files

--- HFmonitoring/Untuplizer/run_data.C
+++ HFmonitoring/Untuplizer/run_data.C
@@ -2,11 +2,17 @@
   gROOT->LoadMacro("xAna_data.C+");
   
   const char* inpaths[] = {
-     "file:/cms/heindl/2018/HF_calibration/EGamma/crab_EGamma_Run2018A/181127_103957/ggTree.root",
-     "file:/cms/heindl/2018/HF_calibration/EGamma/crab_EGamma_Run2018B/181127_104024/ggTree.root",
-     "file:/cms/heindl/2018/HF_calibration/EGamma/crab_EGamma_Run2018C/181127_104055/ggTree.root",
-     "file:/cms/heindl/2018/HF_calibration/EGamma/crab_EGamma_Run2018D/181127_130219/ggTree.root"
-     };
+    "/eos/user/j/jnatoli/HFCalib/2022HF/EGamma/ggTree_EGamma_Run2022B_0000.root",
+    "/eos/user/j/jnatoli/HFCalib/2022HF/EGamma/ggTree_EGamma_Run2022C_0000.root",
+    "/eos/user/j/jnatoli/HFCalib/2022HF/EGamma/ggTree_EGamma_Run2022C_0001.root",
+    "/eos/user/j/jnatoli/HFCalib/2022HF/EGamma/ggTree_EGamma_Run2022D_0000.root"
+    // "davs://cmsxrootd.hep.wisc.edu:1094//store/user/jnatoli/hadded/2022HF/EGamma/ggTree_EGamma_Run2022B_230427_102334_0000.root",
+    // "davs://cmsxrootd.hep.wisc.edu:1094//store/user/jnatoli/hadded/2022HF/EGamma/ggTree_EGamma_Run2022C_230427_133150_0000.root",
+    // "davs://cmsxrootd.hep.wisc.edu:1094//store/user/jnatoli/hadded/2022HF/EGamma/ggTree_EGamma_Run2022C_230427_133150_0001.root",
+    // "davs://cmsxrootd.hep.wisc.edu:1094//store/user/jnatoli/hadded/2022HF/EGamma/ggTree_EGamma_Run2022D_230427_133210_0000.root"
+    // "/eos/user/j/jnatoli/HFCalib/2022HF/EGamma/ggTree_EGamma_Run2022B_230427_102334_0000.root"    
+    // "file:/cms/heindl/2018/HF_calibration/EGamma/crab_EGamma_Run2018D/181127_130219/ggTree.root"
+  };
   
-     xAna_data(inpaths, 4);
+  xAna_data(inpaths, 4);
 }
--- HFmonitoring/Untuplizer/run_mc.C
+++ HFmonitoring/Untuplizer/run_mc.C
@@ -2,8 +2,10 @@
   gROOT->LoadMacro("xAna_mc.C+");
   
   const char* inpaths[] = {
-      "file:/cms/heindl/2018/HF_calibration/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_Fall18/181127_103024/ggTree.root"
+    "/eos/user/j/jnatoli/HFCalib/2022HF/DYJetsToLL_M-50/ggTree_DYJetsToLL_M-50_Winter22_230427_132247_0000.root",
+    "/eos/user/j/jnatoli/HFCalib/2022HF/DYJetsToLL_M-50/ggTree_DYJetsToLL_M-50_Winter22_230427_132606_0000.root"
+    // "file:/cms/heindl/2018/HF_calibration/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/crab_DYJetsToLL_M-50_Fall18/181127_103024/ggTree.root"
   };
 
-  xAna_mc(inpaths, 1);
+  xAna_mc(inpaths, 2);
 }
