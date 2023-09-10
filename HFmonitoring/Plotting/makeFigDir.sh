367b9306b37a9b3c5d762aba5eda374adec2de05
James Natoli <james.vincent.natoli@cern.ch>
2023-09-10 20:58:33 +0200

    logic issues fixed

--- HFmonitoring/Plotting/makeFigDir.sh
+++ HFmonitoring/Plotting/makeFigDir.sh
@@ -1,12 +1,20 @@
+
 #!/bin/bash
 # quick script to create a figures directory...
 
 NEWDIR=${1}
-mkdir ${NEWDIR} || echo "Directory ${NEWDIR} already exists!"; exit
+
+if [[ ! -d ${NEWDIR} ]]; then
+    echo "mkdir ${NEWDIR}"
+    mkdir ${NEWDIR}
+else
+    echo "Directory ${NEWDIR} already exists... will not overwrite"
+fi
+
 for i in Eta EtaBin EtaPhiFit PU Phi; do 
-    if [[ -d ${NEWDIR}/${i} ]]; then 
+    if [[ ! -d ${NEWDIR}/${i} ]]; then 
 	mkdir ${NEWDIR}/${i}; 
     else
-	echo "Directory ${NEWDIR}/${i} already exists!"
+	echo "Directory ${NEWDIR}/${i} already exists, will not overwrite"
     fi
 done
