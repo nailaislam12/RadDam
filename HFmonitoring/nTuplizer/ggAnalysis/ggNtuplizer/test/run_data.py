c85d78691039ecbdffc170d72c5a11a456c4904f
James Natoli <james.vincent.natoli@cern.ch>
2023-10-31 19:40:54 +0100

    2023 global tag

--- HFmonitoring/nTuplizer/ggAnalysis/ggNtuplizer/test/run_data.py
+++ HFmonitoring/nTuplizer/ggAnalysis/ggNtuplizer/test/run_data.py
@@ -12,17 +12,18 @@ process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '124X_dataRun3_Prompt_v10')
+process.GlobalTag = GlobalTag(process.GlobalTag, '130X_dataRun3_Prompt_v3')
+# process.GlobalTag = GlobalTag(process.GlobalTag, '124X_dataRun3_Prompt_v10')
 
 # process.GlobalTag = GlobalTag(process.GlobalTag, '102X_dataRun2_Sep2018Rereco_v1')
 # process.GlobalTag = GlobalTag(process.GlobalTag, '102X_dataRun2_Prompt_v11')
 # process.GlobalTag = GlobalTag(process.GlobalTag, '102X_dataRun2_Prompt_v14'
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-# process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+# process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 250
 
-process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring('/store/data/Run2022B/EGamma/MINIAOD/10Dec2022-v1/2820000/00ca2351-c1c1-4066-b088-0079e861b29a.root'))
+process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring('/store/data/Run2023C/EGamma0/MINIAOD/22Sep2023_v1-v1/2530000/0180e051-34b5-47a0-a851-665aa47846fd.root'))
 
 process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff" )
 process.load("PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff" )
@@ -31,15 +32,15 @@ process.TFileService = cms.Service("TFileService", fileName = cms.string('ggTree
 
 from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
 updateJetCollection(
-    process,
-    jetSource = cms.InputTag('slimmedJets'),
-    labelName = 'UpdatedJEC',
-    jetCorrections = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute','L2L3Residual']), 'None')
-    )
+  process,
+  jetSource = cms.InputTag('slimmedJets'),
+  labelName = 'UpdatedJEC',
+  jetCorrections = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute','L2L3Residual']), 'None')
+)
 
 process.load("ggAnalysis.ggNtuplizer.ggNtuplizer_miniAOD_cfi")
 
 process.p = cms.Path(
-    process.ggNtuplizer
-    )
+  process.ggNtuplizer
+)
 
