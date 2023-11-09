ff1ace128e1bd4cee3a2ec1abf07ed0b6edcb4f6
James Natoli <james.vincent.natoli@cern.ch>
2023-11-09 12:44:42 +0100

    updated Condor Setup

---
+++ HFmonitoring/Untuplizer/for_condor_xanadata.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# condor executable for xAna_data.C
+
+# Collect arguments
+CLUSTERID=${1}
+PROCID=${2}
+ERA=${3}
+CHUNKLIST=$(basename ${4})
+
+OUTFILE=output_data.root
+URL=davs://cmsxrootd.hep.wisc.edu:1094/
+COPYDIR=/store/user/jnatoli/Untuplizer/${ERA}
+
+# Run executable
+echo "Executing xanadata"
+./xanadata ${CHUNKLIST}
+
+# Copy Output
+if [ -f ${OUTFILE} ]; then
+    echo "Copying output: ${URL}/${COPYDIR}/${OUTFILE}_${ERA}_${PROCID}.root"
+    env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-copy -pf ${OUTFILE} ${URL}/${COPYDIR}/output_data_${ERA}_${CLUSTERID}_${PROCID}.root
+else
+    echo "ERROR: No outfile created!!!" 
+fi
