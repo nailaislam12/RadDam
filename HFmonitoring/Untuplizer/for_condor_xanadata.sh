8010f2c58d810661e8974ad19bcf6e053740f6a9
James Natoli <james.vincent.natoli@cern.ch>
2023-11-10 18:15:24 +0100

    updates for /eos/

--- HFmonitoring/Untuplizer/for_condor_xanadata.sh
+++ HFmonitoring/Untuplizer/for_condor_xanadata.sh
@@ -9,8 +9,6 @@ CHUNKLIST=$(basename ${4})
 
 OUTFILE=output_data.root
 COPYDIR=${5}
-# URL=davs://cmsxrootd.hep.wisc.edu:1094/
-# COPYDIR=/store/user/jnatoli/Untuplizer/${ERA}
 
 # Run executable
 echo "Executing xanadata"
@@ -19,7 +17,8 @@ echo "Executing xanadata"
 # Copy Output
 if [ -f ${OUTFILE} ]; then
     echo "Copying output: ${URL}/${COPYDIR}/${OUTFILE}_${ERA}_${PROCID}.root"
-    env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-copy -pf ${OUTFILE} ${URL}/${COPYDIR}/output_data_${ERA}_${CLUSTERID}_${PROCID}.root
+    echo ">>> env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-copy -pf ${OUTFILE} ${COPYDIR}/output_data_${ERA}_${CLUSTERID}_${PROCID}.root"
+    env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-copy -pf ${OUTFILE} ${COPYDIR}/output_data_${ERA}_${CLUSTERID}_${PROCID}.root
 else
     echo "ERROR: No outfile created!!!" 
 fi
--- HFmonitoring/Untuplizer/submitUntuple.sh
+++ HFmonitoring/Untuplizer/submitUntuple.sh
@@ -5,19 +5,27 @@
 # TODO: figure out procedure for if jobs fail?
 
 # Make these vars available to the condor submission script
-# export url=davs://cmsxrootd.hep.wisc.edu:1094
-export url="" 
-export era=EGamma0_Run2023B_0000
-# export copydir=${url}/store/user/jnatoli/Untuplizer/${era}
+
+url="" 
+# url=davs://cmsxrootd.hep.wisc.edu:1094
 export copydir=${url}/eos/user/j/jnatoli/Untuplizer/${era}
+export era=EGamma0_Run2023B_0000
 
 RCOMP="`root-config --cflags --glibs`"
 macro=xAna_data.C
 bin=xanadata
 
-# compile the code
+# Check if environment is clean
+if [ ! -z ${CMSSW_BASE} ]; then
+  echo -e "\tError: This script must be run in a clean environment. You already have a CMSSW environment set up for ${CMSSW_VERSION}."
+  echo -e "\tPlease try again from a clean shell."
+  exit 1
+fi
+
+# Compile the code
+echo ">>> Compiling macro: ${macro}" 
 if ! $(g++ $RCOMP $macro -o $bin); then
-    echo "Couldn't compile ${macro}... exiting"
+    echo -e "\tCouldn't compile ${macro}... exiting"
     exit 1
 fi
 
@@ -32,60 +40,64 @@ chunkdir=${era}/infiles
 filesInChunk=100
 
 # Create directories as necessary
+echo ">>> Checking directories: ${era}" 
 if [ ! -d ${era} ]; then
-    echo "creating directory ${era}..."
+    echo -e "\tcreating directory ${era}..."
     mkdir ${era}
 fi
 
 if [ ! -d ${chunkdir} ]; then
-    echo "creating directory ${chunkdir}..."
+    echo -e "\tcreating directory ${chunkdir}..."
     mkdir ${chunkdir}
 fi
 
 if [ ! -d ${era}/jobs ]; then
-    echo "creating jobs directories..."
+    echo -e "\tcreating jobs directories..."
     mkdir ${era}/jobs
 fi
 
 if [ ! -d ${era}/jobs/err ]; then
-    echo "creating err directories..."
+    echo -e "\tcreating err directories..."
     mkdir ${era}/jobs/err
 fi
 
 if [ ! -d ${era}/jobs/out ]; then
-    echo "creating out directories..."
+    echo -e "\tcreating out directories..."
     mkdir ${era}/jobs/out
 fi
 
 if [ ! -d ${era}/jobs/log ]; then
-    echo "creating log directories..."
+    echo -e "\tcreating log directories..."
     mkdir ${era}/jobs/log
 fi
 
 # Check for existing chunks
+echo ">>> Checking for chunks: ${chunkdir}" 
 # NB: &> redirects stderr and stdout, > just stdout
 if ls ${chunkdir}/*.txt &> /dev/null; then
-    echo "Old chunks exist... deleting"
+    echo -e "\tOld chunks exist... deleting"
     rm ${chunkdir}/*.txt
 fi
 
 # Look for the files
+echo ">>> Looking for files: ${indir}" 
 if ! env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-ls ${indir} > /dev/null; then
-    echo "Unable to access files: ${indir}"
-    echo "Exiting..."
+    echo -e "\tUnable to access files: ${indir}"
+    echo -e "\tExiting..."
     exit 1
 fi
 
 # Make the chunks
-echo "Creating chunk: ${chunk}"
+echo ">>> Creating chunk: ${chunk}"
 for file in $( env -i X509_USER_PROXY=${X509_USER_PROXY} gfal-ls ${indir}); do
     echo ${indir}/${file} >> ${chunkdir}/chunk_${chunk}.txt
     if [ $((${counter} % ${filesInChunk})) -eq 0 ]; then
 	chunk=$((${chunk} + 1))
-	echo "Creating chunk: ${chunk}"
+	echo ">>> Creating chunk: ${chunk}"
     fi
     counter=$((${counter} + 1))
 done
 
 # Submit jobs!
+echo ">>> Submitting jobs: condor_submit era=${era} condor_submit.sub"
 condor_submit era=${era} condor_submit.sub
