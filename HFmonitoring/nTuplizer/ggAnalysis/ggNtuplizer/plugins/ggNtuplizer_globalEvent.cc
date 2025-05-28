6a69b2060a9780e3e9b90445c6841c90a591e4b3
Naila Islam <148743519+nailaislam12@users.noreply.github.com>
2025-05-28 15:48:08 +0200

    Update ggNtuplizer_globalEvent.cc

--- HFmonitoring/nTuplizer/ggAnalysis/ggNtuplizer/plugins/ggNtuplizer_globalEvent.cc
+++ HFmonitoring/nTuplizer/ggAnalysis/ggNtuplizer/plugins/ggNtuplizer_globalEvent.cc
@@ -13,6 +13,11 @@ Int_t       nVtx_;
 float       rho_;
 float       rhoCentral_;
 
+//double      L1ECALPrefire_;
+//double      L1ECALPrefireUp_;
+//double      L1ECALPrefireDown_;
+ULong64_t   HLTEleMuX_;
+
 void ggNtuplizer::branchesGlobalEvent(TTree* tree) {
 
   tree->Branch("run",     &run_);
@@ -21,7 +26,10 @@ void ggNtuplizer::branchesGlobalEvent(TTree* tree) {
   tree->Branch("nVtx",                 &nVtx_);
   tree->Branch("rho",                  &rho_);
   tree->Branch("rhoCentral",           &rhoCentral_);
-
+  tree->Branch("HLTEleMuX",            &HLTEleMuX_);
+  /*tree->Branch("L1ECALPrefire",        &L1ECALPrefire_);
+  tree->Branch("L1ECALPrefireUp",      &L1ECALPrefireUp_);
+  tree->Branch("L1ECALPrefireDown",    &L1ECALPrefireDown_);*/
 }
 
 void ggNtuplizer::fillGlobalEvent(const edm::Event& e, const edm::EventSetup& es) {
@@ -52,5 +60,63 @@ void ggNtuplizer::fillGlobalEvent(const edm::Event& e, const edm::EventSetup& es
     }
   } 
   else edm::LogWarning("ggNtuplizer") << "Primary vertices info not unavailable";
+  
+  /*// L1 ECAL prefiring
+  L1ECALPrefire_     = 1;
+  L1ECALPrefireUp_   = 1;
+  L1ECALPrefireDown_ = 1;
 
+  if (L1ECALPrefire_) {
+    edm::Handle<double> theprefweight;
+    e.getByToken(prefweight_token_, theprefweight);
+    L1ECALPrefire_    = (*theprefweight);
+
+    edm::Handle<double> theprefweightup;
+    e.getByToken(prefweightup_token_, theprefweightup);
+    L1ECALPrefireUp_   = (*theprefweightup);
+
+    edm::Handle< double > theprefweightdown;
+    e.getByToken(prefweightdown_token_, theprefweightdown);
+    L1ECALPrefireDown_ = (*theprefweightdown);
+  } */
+
+  // HLT treatment
+  HLTEleMuX_            = 0;
+  
+  edm::Handle<edm::TriggerResults> trgResultsHandle;
+  e.getByToken(trgResultsLabel_, trgResultsHandle);
+  
+  /*bool cfg_changed = true;
+  hltPrescaleProvider_.init(e.getRun(), es, trgResultsProcess_, cfg_changed);
+  HLTConfigProvider const&  hltCfg = hltPrescaleProvider_.hltConfigProvider();
+  const int prescaleSet = hltPrescaleProvider_.prescaleSet(e,es);*/
+
+  const edm::TriggerNames &trgNames = e.triggerNames(*trgResultsHandle);
+
+  for (size_t i = 0; i < trgNames.size(); ++i) {
+    const string &name = trgNames.triggerName(i);
+
+    // HLT name => bit correspondence
+    int bitEleMuX = -1;
+    //int bitPho    = -1;
+    //int bitJet    = -1;
+    
+      if      (name.find("HLT_Ele32_WPTight_Gsf_v")                      != string::npos) bitEleMuX =  0;
+      else if (name.find("HLT_Ele30_WPTight_Gsf_v")                      != string::npos) bitEleMuX =  1; 
+      else if (name.find("HLT_Ele38_WPTight_Gsf_v")                      != string::npos) bitEleMuX =  2;
+      else if (name.find("HLT_Ele40_WPTight_Gsf_v")                      != string::npos) bitEleMuX =  3; 
+      else if (name.find("HLT_Ele35_WPTight_Gsf_v")             	 != string::npos) bitEleMuX = 4;
+	
+    // indicates prescaling and whether trigger was fired or not
+    //ULong64_t isPrescaled = (hltCfg.prescaleValue(prescaleSet, name)!=1) ? 1 : 0;
+    ULong64_t isFired     = (trgResultsHandle->accept(i)) ? 1 : 0;
+    //ULong64_t isrejectedByHLTPS = (hltCfg.moduleType(hltCfg.moduleLabel(i,trgResultsHandle->index(i)))=="HLTPrescaler") ? 1: 0;
+
+    if (bitEleMuX >= 0) {
+      HLTEleMuX_            |= (isFired << bitEleMuX);
+      //HLTEleMuXIsPrescaled_ |= (isPrescaled << bitEleMuX);
+    }
+    
+    	  
+  }
 }
